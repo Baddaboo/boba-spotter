f5d97439173ce6ad75bb6f3935180bcfaeb0de62
Blake Tsuzaki <blake589t@gmail.com>
2019-02-23 17:38:12 -0800

    Updates

---
+++ precache-manifest.39342d934726eb136c530e0e4c42ee0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e5140a847aacc7792ce",
+    "url": "/boba-spotter/static/css/main.9ead6578.chunk.css"
+  },
+  {
+    "revision": "8e5140a847aacc7792ce",
+    "url": "/boba-spotter/static/js/main.8e5140a8.chunk.js"
+  },
+  {
+    "revision": "ede61723c5cb97dfd2c7",
+    "url": "/boba-spotter/static/js/runtime~main.ede61723.js"
+  },
+  {
+    "revision": "7fef14d580165cf0a542",
+    "url": "/boba-spotter/static/js/2.7fef14d5.chunk.js"
+  },
+  {
+    "revision": "c18c8f283b03030364b18830368002b0",
+    "url": "/boba-spotter/index.html"
+  }
+];
