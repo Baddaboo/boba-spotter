d9d51f9816c769034fb6e02c247b4c4fc086eadc
Blake Tsuzaki <blake589t@gmail.com>
2019-02-23 17:50:18 -0800

    Updates

--- precache-manifest.98aa7a56a9c9e753fc570adfeec25e8a.js
+++ precache-manifest.98aa7a56a9c9e753fc570adfeec25e8a.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "0a5544ed30f763a50fb0",
+    "revision": "2f399f4039e6c5d329af",
     "url": "/boba-spotter/static/css/main.9ead6578.chunk.css"
   },
   {
-    "revision": "0a5544ed30f763a50fb0",
-    "url": "/boba-spotter/static/js/main.0a5544ed.chunk.js"
+    "revision": "2f399f4039e6c5d329af",
+    "url": "/boba-spotter/static/js/main.2f399f40.chunk.js"
   },
   {
     "revision": "ede61723c5cb97dfd2c7",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/boba-spotter/static/js/2.7fef14d5.chunk.js"
   },
   {
-    "revision": "bc0f689637bfdb37d94b0de79b1f129b",
+    "revision": "f84cacf280718cf3df0e44b92f1a7dff",
     "url": "/boba-spotter/index.html"
   }
 ];
