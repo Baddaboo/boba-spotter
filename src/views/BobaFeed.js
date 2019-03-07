35312618febc2aa872d24e53f4fa1fc9473d41ba
Blake Tsuzaki <blake589t@gmail.com>
2019-03-06 16:30:06 -0800

    added maps and posts

--- src/views/BobaFeed.js
+++ src/views/BobaFeed.js
@@ -1,28 +1,56 @@
 import React, { Component } from 'react';
-import { withStyles, Typography } from '@material-ui/core';
-import classNames from 'classname';
+import { withStyles, Card, Typography } from '@material-ui/core';
 
 import '../styles/global.css';
+import Axios from 'axios';
 
-const styles = {
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-}
+const styles = theme => ({
+  card: {
+    padding: 20,
+    margin: 10
+  },
+  toolbar: theme.mixins.toolbar,
+});
 
 class BobaFeed extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: []
+    }
+  }
+
+  getPosts = () => {
+    Axios
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        this.setState({ posts: response.data });
+      })
+      .catch(error => console.log(error));
+  }
+
+  componentDidMount() {
+    this.getPosts();
+  }
+
   render() {
     const { classes } = this.props;
+    const { posts } = this.state;
+    
     return (
-      <div className={classNames('fillParent', classes.container)}>
-        <Typography
-          variant="h6" 
-          color="inherit">
-        Feed
-        </Typography>
+      <div className="fillParent">
+        <div className={classes.toolbar} />
+        { posts.map(post =>
+          <Card className={ classes.card }>
+            <Typography color="textSecondary" gutterBottom>
+              { post.title }
+            </Typography>
+            <Typography component="p">
+              { post.body }
+            </Typography>
+          </Card>
+        ) }
       </div>
     );
   }
--- src/views/BobaMap.js
+++ src/views/BobaMap.js
@@ -1,31 +1,100 @@
 import React, { Component } from 'react';
-import { withStyles, Typography } from '@material-ui/core';
-import classNames from 'classname';
+import { withStyles, LinearProgress } from '@material-ui/core';
+import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
 
 import '../styles/global.css';
+import MapProvider from '../providers/MapProvider';
+import bobaResponse from './boba-locations.json';
 
-const styles = {
+const styles = theme => ({
   container: {
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
-    alignItems: 'center'
-  }
-}
+    alignItems: 'center',
+    height: '100%'
+  },
+  map: {
+    height: '100%',
+  },
+  toolbar: theme.mixins.toolbar,
+});
+
+const googleApiKey = "AIzaSyAWIgRoSnkMs-IfdnB3vIvwaW35bkqb7bc";
 
 class BobaMap extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showLoader: false,
+      mapCenter: { lat: 40.854885, lng: -88.081807 },
+      bobaMarkers: []
+    }
+  }
+
+  markersForBusinesses = (businesses) => {
+    return businesses.map(business => (
+      <Marker
+        name={ business.name }
+        position={{
+          lat: business.coordinates.latitude,
+          lng: business.coordinates.longitude
+        }}
+        icon={{
+          url: business.image_url,
+          anchor: new this.props.google.maps.Point(32,32),
+          scaledSize: new this.props.google.maps.Size(64,64)
+        }} />
+    ));
+  }
+
+  componentDidMount() {
+    MapProvider
+      .getCurrentLocation()
+      .then(position => {
+        let mapCenter = {
+          lat: position.latitude,
+          lng: position.longitude
+        }
+
+        let bobaPlaces = bobaResponse.businesses;
+        let bobaMarkers = this.markersForBusinesses(bobaPlaces);
+
+        this.setState({
+          bobaMarkers,
+          mapCenter,
+          showLoader: false,
+        });
+
+        this.setState({
+          mapCenter,
+          showLoader: false
+        });
+      })
+      .catch(error => console.log(error));
+    
+    this.setState({ showLoader: true });
+  }
+
   render() {
     const { classes } = this.props;
     return (
-      <div className={classNames('fillParent', classes.container)}>
-        <Typography
-          variant="h6" 
-          color="inherit">
-        Map
-        </Typography>
+      <div className="fillParent">
+        <div className={classes.toolbar} />
+        { this.state.showLoader  &&  <LinearProgress /> }
+        <Map
+          className={ classes.map }
+          center={ this.state.mapCenter }
+          google={this.props.google} 
+          zoom={14}>
+          { this.state.bobaMarkers }
+        </Map>
       </div>
     );
   }
 }
 
-export default withStyles(styles)(BobaMap);
+export default GoogleApiWrapper({
+  apiKey: googleApiKey
+})(withStyles(styles)(BobaMap));
